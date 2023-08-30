f8ce352361025a6655d88598377ae54caf949669
Maria <maria123321borovikova@gmail.com>
2023-08-30 21:58:47 +0200

    base of the quiz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import quizData from './quizData';
 
 function App() {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [answers, setAnswers] = useState({});
+  const [boxIdeas, setBoxIdeas] = useState(null);
+
+  const handleAnswer = (questionIndex, answer) => {
+    setAnswers((prevAnswers) => ({
+      ...prevAnswers,
+      [questionIndex]: answer,
+    }));
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
+
+  const renderQuestions = () => {
+    if (currentQuestionIndex < quizData.length) {
+      const currentQuestion = quizData[currentQuestionIndex];
+      const options = currentQuestion.options;
+
+      return (
+        <div>
+          <h3>Question {currentQuestionIndex + 1}:</h3>
+          <p>{currentQuestion.question}</p>
+          {options.map((option, index) => (
+            <button key={index} onClick={() => handleAnswer(currentQuestionIndex, option)}>
+              {option}
+            </button>
+          ))}
+        </div>
+      );
+    } else {
+      if (!boxIdeas) {
+        axios.post('http://localhost:3001/api/generate-box-ideas', { answers: Object.values(answers) })
+          .then(response => {
+            setBoxIdeas(response.data);
+          })
+          .catch(error => {
+            console.error('Error generating box ideas:', error);
+          });
+      }
+
+      return (
+        <div>
+          <p>Quiz completed!</p>
+          <h3>Box Ideas for You:</h3>
+          <p>{boxIdeas}</p>
+        </div>
+      );
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Personalized Quiz App</h1>
+      {renderQuestions()}
     </div>
   );
 }
---
+++ src/quizData.js
@@ -0,0 +1,104 @@
+const quizData = [
+    {
+        question: "How do you typically spend your free time?",
+        options: [
+            "Reading, writing, or engaging with artistic activities.",
+            "Engaging in physical activities like walking, jogging, or yoga.",
+            "Exploring new cuisines and cooking.",
+            "Listening to music or playing a musical instrument.",
+            "Spending time with pets or animals."
+        ]
+    },
+    {
+        question: "What best describes your preferred environment?",
+        options: [
+            "A cozy and quiet space for introspection.",
+            "Outdoors, surrounded by nature and fresh air.",
+            "A well-organized and aesthetically pleasing home.",
+            "A space filled with soothing music or sounds.",
+            "Any environment where I can interact with animals."
+        ]
+    },
+    {
+        question: "What type of media do you find most comforting?",
+        options: [
+            "Books, novels, or poetry.",
+            "Podcasts or guided meditation sessions.",
+            "Cooking shows or food blogs.",
+            "Instrumental music or ambient sounds.",
+            "Animal videos or documentaries."
+        ]
+    },
+    {
+        question: "How do you feel after social interactions?",
+        options: [
+            "Energized and inspired.",
+            "Calmer and more centered.",
+            "Satisfied and content.",
+            "Uplifted and joyful.",
+            "Relaxed and comforted."
+        ]
+    },
+    {
+        question: "What type of scents do you find appealing?",
+        options: [
+            "Earthy or floral scents.",
+            "Fresh and natural scents.",
+            "Warm and comforting scents.",
+            "Clean and crisp scents.",
+            "Natural and outdoor-inspired scents."
+        ]
+    },
+    {
+        question: "Which activity would you enjoy exploring?",
+        options: [
+            "Creative writing or journaling.",
+            "Mindful walking or nature meditation.",
+            "Trying out a new recipe or cooking technique.",
+            "Learning to play a musical instrument.",
+            "Volunteering at an animal shelter or sanctuary."
+        ]
+    },
+    {
+        question: "What helps you relax the most?",
+        options: [
+            "Getting lost in a good book.",
+            "Spending time in nature.",
+            "Preparing and enjoying a delicious meal.",
+            "Listening to soothing music.",
+            "Spending time with animals."
+        ]
+    },
+    {
+        question: "What do you consider a positive accomplishment?",
+        options: [
+            "Completing a creative project.",
+            "Achieving a fitness or health goal.",
+            "Preparing a successful meal from scratch.",
+            "Mastering a musical piece.",
+            "Providing care and comfort to animals."
+        ]
+    },
+    {
+        question: "How do you handle stress?",
+        options: [
+            "By expressing myself through art or writing.",
+            "By practicing deep breathing and relaxation techniques.",
+            "By immersing myself in cooking or baking.",
+            "By listening to calming music.",
+            "By spending time with animals or pets."
+        ]
+    },
+    {
+        question: "Which phrase resonates with you the most?",
+        options: [
+            "\"Imagination is the only weapon in the war against reality.\" - Lewis Carroll",
+            "\"In every walk with nature, one receives far more than he seeks.\" - John Muir",
+            "\"Cooking is like love. It should be entered into with abandon or not at all.\" - Harriet van Horne",
+            "\"Where words fail, music speaks.\" - Hans Christian Andersen",
+            "\"The greatness of a nation can be judged by the way its animals are treated.\" - Mahatma Gandhi"
+        ]
+    }
+];
+
+export default quizData;
